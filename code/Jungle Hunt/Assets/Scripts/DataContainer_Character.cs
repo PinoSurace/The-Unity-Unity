92ea60d4c3716b7f54e50dc2a1320a0bf0e67dae
Samu Mäkinen <samu.makinen@student.tut.fi>
2018-01-23 16:29:11 +0200

    Attempt at saving information between scenes.
    Added a function to the name-input button this saves the data to an object.
    Added an object that stores player information even between scenes. (In theory, not tested)
    Reformated project structure to use folders.

---
+++ code/Jungle Hunt/Assets/Scripts/DataContainer_Character.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataContainer_Character : MonoBehaviour {
+
+	// Keep any changes through transitions.
+	void Start ()
+    {
+        DontDestroyOnLoad(this);
+	}
+
+    public void SetPlayerName(string value)
+    {
+        PlayerName = value;
+    }
+
+    public string GetPlayerName()
+    {
+        return PlayerName;
+    }
+
+    // Store data.
+    string PlayerName = "";
+}
---
+++ code/Jungle Hunt/Assets/Scripts/Save_PlayerName.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Save_PlayerName : MonoBehaviour {
+
+    public DataContainer_Character data;
+    public Text nameinput;
+
+	public void Save()
+    {
+        string playername = nameinput.text;
+        data.SetPlayerName(playername);
+        Debug.Log(string.Concat("Data sent, here's it's load return: ", data.GetPlayerName()));
+	}
+}
