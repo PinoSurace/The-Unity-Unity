052998662f1eaa3bc4c77f384b044022108dcedd
Vili Saura <vili.saura@student.tut.fi>
2018-01-23 20:46:20 +0200

    Added Scene changing to the buttons and a placeholder for level 1

---
+++ code/Jungle Hunt/Assets/Scripts/Next_Scene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Next_Scene : MonoBehaviour {
+
+	//Load the next scene. Ĺevel is the index of the scene in build settings
+
+	public void Change_scene (int level) {
+		SceneManager.LoadScene(level);
+	}
+
+}
