918a4fe72655cdb9a8d7f25e4b6ddb52512a0d79
240264 <vili.saura@student.tut.fi>
2018-02-02 21:55:48 +0200

    Basic animations and jumping controlls

---
+++ code/Jungle Hunt/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+
+    public float playerSpeed;
+    public Vector2 jumpHeight;
+
+    public Rigidbody2D rb;
+    private Animator animator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            rb.AddForce(new Vector2(-2, 10), ForceMode2D.Impulse);
+            animator.SetTrigger("PlayerJump");
+            
+        }
+    }
+}
