5048a5ae1bdaab1fe3a40acaa5e416fa2b1afd1c
240264 <vili.saura@student.tut.fi>
2018-02-07 20:13:50 +0200

    Changed player into state machine. Added Jumping state

--- code/Jungle Hunt/Assets/Scripts/Player.cs
+++ code/Jungle Hunt/Assets/Scripts/Player.cs
@@ -12,24 +12,54 @@ public class Player : MonoBehaviour
     public Rigidbody2D rb;
     private Animator animator;
 
+	enum State
+	{
+		State_Idle,
+		State_Jumping,
+		State_Dead,
+		State_Attacking,
+		State_Swinging,
+		State_Swimming,
+		State_Bubble,
+		State_Walking,
+		State_Crouching
+	}
+
+	private State CurrentState;
+
+	private void ChangeState(string input)
+	{
+		switch (CurrentState) 
+		{
+			case State.State_Idle:
+				if (input == " ") {
+					print("Jee2");
+					CurrentState = State.State_Jumping;
+					//An impulse is used to move the player
+					rb.AddForce (new Vector2 (XAxis, YAxis), ForceMode2D.Impulse);
+					//The jump animation is triggered
+					animator.SetTrigger ("PlayerJump");
+				}
+				break;				
+
+		}
+
+	}
+		
+
     //We create shortcuts for animator and rigidbody
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+		CurrentState = State.State_Idle;
     }
 
     //Pressing the space key makes the player jump.
     private void Update()
     {
-        if (Input.GetKeyDown("space"))
-        {
-            //An impulse is used to move the player
-            rb.AddForce(new Vector2(XAxis, YAxis), ForceMode2D.Impulse);
-            //The jump animation is triggered
-            animator.SetTrigger("PlayerJump");
-            
-            
-        }
+
+		ChangeState (Input.inputString);
+        
     }
 }
