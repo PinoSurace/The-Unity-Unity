67161cdaf7d4db03f394bb0a9e189234224cdf6a
224895 <lassi.rintala@student.tut.fi>
2018-02-14 19:28:23 +0200

    Added collision triggers for the rope elements - Debug prints can be enabled to see if the collisions are triggering

---
+++ code/Jungle Hunt/Assets/Scripts/RopeCollisionTrigger.cs
@@ -0,0 +1,27 @@
+﻿// Uncomment this line to enable debug prints for the class
+//#define DEBUG_ROPE_COLLISION_TRIGGER
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Rope collision trigger class for detecting the player
+ * colliding with the rope.
+ */
+public class RopeCollisionTrigger : MonoBehaviour {
+
+    void OnTriggerEnter2D()
+    {
+#if (DEBUG_ROPE_COLLISION_TRIGGER)
+        print(Time.time.ToString() + ": Another collider entered " + this.name + "!");
+#endif
+    }
+
+    void OnTriggerExit2D()
+    {
+#if (DEBUG_ROPE_COLLISION_TRIGGER)
+        print(Time.time.ToString() + ": Another collider exit " + this.name + "!");
+#endif
+    }
+}
