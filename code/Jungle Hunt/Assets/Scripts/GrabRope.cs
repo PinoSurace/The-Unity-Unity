d10bbfdcfc7a8e65c1bc3759ab3c33b96429e7c6
240264 <vili.saura@student.tut.fi>
2018-02-21 18:35:52 +0200

    Prevented player from grabbing already grabbed ropes and removed momentum the player gained from the rope swing.

--- code/Jungle Hunt/Assets/Scripts/GrabRope.cs
+++ code/Jungle Hunt/Assets/Scripts/GrabRope.cs
@@ -6,17 +6,22 @@ public class GrabRope : MonoBehaviour {
 	//This Script could be added straight to the player script
 
 	//Distance the player keeps from the rope
-	float dist = 0.2f;
+	float dist = 0f;
 	void OnTriggerEnter2D (Collider2D other){
 		//Checking if the collider is a rope and that the player is not swinging
 		if (other.gameObject.transform.parent.name.StartsWith("Rope") && 
-			this.gameObject.GetComponent<Player>().CurrentState != Player.State.State_Swinging)
+			other.gameObject.transform.parent.gameObject.GetComponent<RopeGrabbed>().grabbed ==false)
+			//this.gameObject.GetComponent<Player>().CurrentState != Player.State.State_Swinging
+		
 		{			
 			//We create a new distance joint between the player and the rope
 			var joint = gameObject.AddComponent <DistanceJoint2D>();
 			joint.connectedBody = other.GetComponent<Rigidbody2D>();
 			joint.distance = dist;
 
+			//Set the rope as grabbed
+			other.gameObject.transform.parent.gameObject.GetComponent<RopeGrabbed> ().grabbed = true;
+			//Change the player state to swinging
 			this.gameObject.GetComponent<Player> ().CurrentState = Player.State.State_Swinging;
 
 		}
