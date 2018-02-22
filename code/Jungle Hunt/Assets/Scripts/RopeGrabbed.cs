98451dd848b563b5b27a997a8c0f94bdefd3d4b1
253582 <samu.makinen@student.tut.fi>
2018-02-22 14:43:12 +0200

    Added preliminary camera controls and methods.
    Removed unused animator from player prefab.

--- code/Jungle Hunt/Assets/Scripts/RopeGrabbed.cs
+++ code/Jungle Hunt/Assets/Scripts/RopeGrabbed.cs
@@ -5,4 +5,13 @@ using UnityEngine;
 public class RopeGrabbed : MonoBehaviour {
 
 	public bool grabbed = false;
+
+    private float animtime = 1.5f;
+    private Vector3 offset = new Vector3(-1.2f, 0, 0);
+
+    public void CorrelateCameraToThisObject()
+    {
+        Vector3 target = this.transform.position + offset;
+        GameObject.Find("Main Camera").GetComponent<CameraControls>().GotoTarget(target, animtime);
+    }
 }
