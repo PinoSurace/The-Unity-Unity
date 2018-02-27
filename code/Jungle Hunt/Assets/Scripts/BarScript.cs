65f2f12c34ed6ba7abe26f25b5772049051a6b87
pino surace <pino.surace@student.tut.fi>
2018-02-27 15:14:23 +0200

    air meter, collider for bubble and player done

---
+++ code/Jungle Hunt/Assets/Scripts/BarScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour
+{
+    public Slider slider;  
+    
+    // Use this for initialization
+    void Start()
+    {
+        slider.value = 10;
+        StartCoroutine(HandleBar());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    IEnumerator HandleBar()
+    {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(1);
+            slider.value -= 1;
+            
+            //no more air. dead 
+            if (slider.value == 0)
+            {                
+                Player player = GameObject.Find("Tarzan").GetComponent<Player>();
+                player.DeadlyHazard();
+                break;
+            }
+
+        }
+    }
+
+    
+
+
+}
---
+++ code/Jungle Hunt/Assets/Water_Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Water_Script : MonoBehaviour {
+    public Slider slider;
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("collision name = " + other.gameObject.name);
+        if (other.gameObject.name == "Tarzan")
+        {            
+            slider.value = 10;
+            
+        }
+
+        if (other.gameObject.name == "bubble(Clone)")
+        {
+
+            Destroy(other.gameObject);
+
+        }
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Tarzan")
+        {
+            slider.value = 10;
+        }
+    }
+}
