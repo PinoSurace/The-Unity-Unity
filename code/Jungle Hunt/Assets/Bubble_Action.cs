1be3b81e8ddf5431385b7517f6ba17fca87b7939
pino surace <pino.surace@student.tut.fi>
2018-02-28 18:14:36 +0200

    bubble collider finished and added lower collider level 2

--- code/Jungle Hunt/Assets/Bubble_Action.cs
+++ code/Jungle Hunt/Assets/Bubble_Action.cs
@@ -5,48 +5,46 @@ using UnityEngine;
 public class Bubble_Action : MonoBehaviour {
 
     private bool player_In = false;
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        //Debug.Log("collision name = " + other.gameObject.name);
-        if (other.gameObject.name == "Tarzan")
-        {
-            Player player = other.gameObject.GetComponent<Player>();
-            player.CurrentState = Player.State.State_Bubble;
-            player.transform.position = this.gameObject.transform.position;
-            player.transform.Translate(0, 1, this.gameObject.GetComponent<Rigidbody2D>().velocity.y * 0.000001f);
-            this.gameObject.transform.Translate(0, 1, this.gameObject.GetComponent<Rigidbody2D>().velocity.y * 0.000001f);
-            //player.GetComponent<Rigidbody2D>().velocity = this.gameObject.GetComponent<Rigidbody2D>().velocity;
-            player_In = true;
-        }
+    private Player player;
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
         
-
-    }
-
-    /*private void OnDestroy()
-    {
-        Player player = GameObject.Find("Tarzan").GetComponent<Player>();
-        if(player_In == true)
-        {
-            player.CurrentState = Player.State.State_Swimming;
-        }
-        
-    }*/
-
-    
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
         if (collision.gameObject.name == "Tarzan")
         {
-            Player player = collision.gameObject.GetComponent<Player>();
-            player.transform.position = this.gameObject.transform.position;
-            player.transform.Translate(0,1, this.gameObject.GetComponent<Rigidbody2D>().velocity.y*0.1f);
-            this.gameObject.transform.Translate(0,1, this.gameObject.GetComponent<Rigidbody2D>().velocity.y * 0.1f);
-            //player.transform.Translate(0, -1, this.gameObject.GetComponent<Rigidbody2D>().velocity.y * 0.0000009f);
-            //this.gameObject.transform.Translate(0, -1, this.gameObject.GetComponent<Rigidbody2D>().velocity.y * 0.0000009f);
-            //player.GetComponent<Rigidbody2D>().velocity = this.gameObject.GetComponent<Rigidbody2D>().velocity;
+            player = collision.gameObject.GetComponent<Player>();            
+            player.InTheBubble();
+            player.transform.position = this.gameObject.transform.position;            
+            player.GetComponent<Rigidbody2D>().velocity = this.gameObject.GetComponent<Rigidbody2D>().velocity;
             player_In = true;
         }
+        else if(collision.gameObject.name == "WaterLimit")
+        {
+            Destroy(gameObject);
+        }
     }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Tarzan" && player_In == true)
+        {
+            player = collision.gameObject.GetComponent<Player>();            
+            player.GetComponent<Rigidbody2D>().velocity = this.gameObject.GetComponent<Rigidbody2D>().velocity;            
+        }
+    }  
+   
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.name == "Tarzan")
+        {
+            if (player_In == true)
+            {
+                player.OutOfBubble();                
+            }
+
+        }
+    }
+    
 }
