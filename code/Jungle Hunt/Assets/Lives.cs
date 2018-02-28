1dcaccdf556fed5200578b726f6a19be5821ee2e
pino surace <pino.surace@student.tut.fi>
2018-02-28 15:52:08 +0200

    added some bubble_things

---
+++ code/Jungle Hunt/Assets/Lives.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lives : MonoBehaviour {
+
+    public Text number_of_lives;
+	// Use this for initialization
+	void Start () {
+        number_of_lives.text = "5";
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //number_of_lives.text = GameObject.Find("")
+
+    }
+}
