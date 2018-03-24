8ead36cf1e9eb4a961f06db47da78fc42878a8c3
pino surace <pino.surace@student.tut.fi>
2018-03-24 17:28:57 +0200

    added cannibal movement, layers to improve it and polishing of background
    Req: 9.2

---
+++ code/Jungle Hunt/Assets/Scripts/Cannibal_Script.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannibal_Script : MonoBehaviour
+{
+    public SpriteRenderer head_sprite;
+    public SpriteRenderer body_sprite;
+    public Rigidbody2D r;
+    private float speed;    
+    int direction;
+    float maxDist;
+    float minDist;
+    
+    // Use this for initialization
+    void Start()
+    {
+        
+        r = GetComponent<Rigidbody2D>();
+        speed = 1.0f; 
+        direction = 1;
+        maxDist =5 ;
+        minDist = -5;
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //r.AddForce(new Vector2(speed * direction, 0.0f));
+        r.velocity = new Vector2(speed * direction, 0.0f);
+        if (r.OverlapPoint(new Vector2(maxDist, r.position.y)))
+        {
+            //r.transform.eulerAngles = new Vector3(0, 180, 0);
+            head_sprite.flipX = !head_sprite.flipX;
+            body_sprite.flipX = !body_sprite.flipX;
+            direction = -1;    
+        }
+        else if (r.OverlapPoint(new Vector2(minDist, r.position.y)))
+        {
+            head_sprite.flipX = !head_sprite.flipX;
+            body_sprite.flipX = !body_sprite.flipX;
+            direction = 1;            
+        }
+        
+    }
+}
