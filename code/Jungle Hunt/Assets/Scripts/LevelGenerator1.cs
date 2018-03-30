2cff382fcf05a13f23fdfb0fc9bba1517c21df97
224895 <lassi.rintala@student.tut.fi>
2018-03-30 20:13:43 +0300

    Reversed level 1 direction
    Req: X

--- code/Jungle Hunt/Assets/Scripts/LevelGenerator1.cs
+++ code/Jungle Hunt/Assets/Scripts/LevelGenerator1.cs
@@ -76,12 +76,12 @@ public class LevelGenerator1 : MonoBehaviour {
         var ropeDistance = 0;
         for (int n = 0; n < numberOfRopes; n++)
         {
-            var rope = Instantiate(ropePrefab, new Vector3(lastRopeX - ropeDistance, ropeY, 97), Quaternion.identity);
+            var rope = Instantiate(ropePrefab, new Vector3(lastRopeX + ropeDistance, ropeY, 97), Quaternion.identity);
 
             // We also need to move the connected anchor of the first element's hinge joint to the correct place
             var firstElement = rope.Find("rope_1");
             firstElement.gameObject.GetComponent<HingeJoint2D>().connectedAnchor
-                = new Vector2(lastRopeX - ropeDistance - ropeJointXOffset, ropeY + ropeJointYOffset);
+                = new Vector2(lastRopeX + ropeDistance - ropeJointXOffset, ropeY + ropeJointYOffset);
 
             // Set a random speed (gravity scale) for each rope
             var ropeSpeed = Random.Range(ropeMinSpeed, ropeMaxSpeed);
@@ -91,7 +91,7 @@ public class LevelGenerator1 : MonoBehaviour {
                 ropeElement.gameObject.GetComponent<Rigidbody2D>().gravityScale = ropeSpeed;
             }
 
-            lastRopeX -= ropeDistance;
+            lastRopeX += ropeDistance;
             ropeDistance = Random.Range(ropeMinDistance, ropeMaxDistance);
         }
     }
@@ -102,7 +102,7 @@ public class LevelGenerator1 : MonoBehaviour {
     void GenerateBackground()
     {
         float backgroundImageWidth = backgroundPrefab.GetComponent<SpriteRenderer>().bounds.size.x;
-        for (float x = 0; x > lastRopeX - levelEndLength - backgroundImageWidth; x -= backgroundImageWidth)
+        for (float x = 0; x < lastRopeX + levelEndLength + backgroundImageWidth; x += backgroundImageWidth)
         {
             Instantiate(backgroundPrefab, new Vector3(x, 0, 100), Quaternion.identity);
         }
@@ -114,7 +114,7 @@ public class LevelGenerator1 : MonoBehaviour {
     void GenerateFoliage()
     {
         float leavesImageWidth = leavesPrefab.GetComponent<SpriteRenderer>().bounds.size.x;
-        for (float x = 0; x > lastRopeX - levelEndLength - leavesImageWidth; x -= leavesImageWidth)
+        for (float x = 0; x < lastRopeX + levelEndLength + leavesImageWidth; x += leavesImageWidth)
         {
             Instantiate(leavesPrefab, new Vector3(x, 4.5f, 94), Quaternion.identity);
         }
@@ -123,8 +123,8 @@ public class LevelGenerator1 : MonoBehaviour {
         // water to start from an exact place at the end of the scene and not be defined by a
         // multiple of the grass image width
         float grassImageWidth = grassPrefab.GetComponent<SpriteRenderer>().bounds.size.x;
-        float grassStartX = lastRopeX + grassImageWidth / 2.0f - ropeJointXOffset;
-        for (float x = grassStartX; x < grassImageWidth; x += grassImageWidth)
+        float grassStartX = lastRopeX - grassImageWidth / 2.0f - ropeJointXOffset;
+        for (float x = grassStartX; x > -grassImageWidth; x -= grassImageWidth)
         {
             Instantiate(grassPrefab, new Vector3(x, -3.5f, -2), Quaternion.identity);
         }
@@ -135,18 +135,18 @@ public class LevelGenerator1 : MonoBehaviour {
      */
     void GenerateLevelEnd()
     {
-        Instantiate(waterPrefab, new Vector3(lastRopeX - levelEndLength/2.0f - ropeJointXOffset, -4.0f, 94), Quaternion.identity);
+        Instantiate(waterPrefab, new Vector3(lastRopeX + levelEndLength/2.0f - ropeJointXOffset, -4.0f, 94), Quaternion.identity);
 
         GameObject gameObject = new GameObject("DivingAnimationCollider");
         BoxCollider2D collider = gameObject.AddComponent<BoxCollider2D>();
         collider.size = new Vector2(levelEndLength, 10.0f);
         collider.isTrigger = true;
-        collider.transform.position = new Vector3(lastRopeX - levelEndLength / 2.0f - ropeJointXOffset, 0, 0);
+        collider.transform.position = new Vector3(lastRopeX + levelEndLength / 2.0f - ropeJointXOffset, 0, 0);
 
         gameObject = new GameObject("NextLevelCollider");
         collider = gameObject.AddComponent<BoxCollider2D>();
         collider.size = new Vector2(levelEndLength, 1.0f);
         collider.isTrigger = true;
-        collider.transform.position = new Vector3(lastRopeX - levelEndLength / 2.0f - ropeJointXOffset, -5.5f, 0);
+        collider.transform.position = new Vector3(lastRopeX + levelEndLength / 2.0f - ropeJointXOffset, -5.5f, 0);
     }
 }
